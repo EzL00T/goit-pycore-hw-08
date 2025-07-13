71cac12d6d74b0f351a550860736ce216358348d
Yurii Kroitor <yuriikroitor98@gmail.com>
2025-07-13 16:28:00 +0300

    Create storage.py

---
+++ storage.py
@@ -0,0 +1,13 @@
+import pickle
+
+def save_data(book, filename="addressbook.pkl"):
+    with open(filename, "wb") as f:
+        pickle.dump(book, f)
+
+def load_data(filename="addressbook.pkl"):
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        from addressbook import AddressBook
+        return AddressBook()
